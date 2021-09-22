18ca2b61cd30b0a09833bda566de4ed928182cbd
jfustolojr <59927884+jfustolojr@users.noreply.github.com>
2021-09-22 18:48:10 -0400

    Add files via upload

---
+++ week3/dns_resolver.ps1
@@ -0,0 +1,7 @@
+﻿param($network, $server)
+foreach ($i in 0..255) {
+  $hosts="$network.$i"
+  foreach ($temp in $hosts) { 
+    Resolve-DnsName -DnsOnly $temp -Server $server -ErrorAction Ignore
+  }
+}
